0810d98f58a91871e5d046cc8d2dd29738c2af60
dlwlrja <kazakovavladillava@gmail.com>
2024-03-24 10:17:48 +0100

    ;\

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Увольнение сотрудников");
+        int i = 2;
+        while (i <=50){
+            System.out.println("Уволен работник с id "+ i);
+            i += 2; //i = i + 2
+        }
+        /*for (int i = 2; i<=50; i = i+2){
+                System.out.println("Уволен работник с id "+ i);
+
+        }
+/*int i = 2;
+        for (i = 2; i >= 100;i++);
+        System.out.println("Уволен сотрудник с id " + i); */
+    }
+}
